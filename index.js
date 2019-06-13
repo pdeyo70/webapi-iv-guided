158a7dd2ab15b9fc633382355ad06218f662002e
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-12 21:51:46 -0500

    server functional, about to connect to heroku

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 4010;
+const greeting = process.env.GREETING;
+
+server.listen(4010, () => {
+  console.log(`\n*** ${greeting} Server Running on http://localhost:4010 ***\n`);
 });
