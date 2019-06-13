53eab5b7606ed320646bf09b618b54b78fa76831
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-12 22:11:38 -0500

    got port dynamically working

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = require('./api/server.js');
 const port = process.env.PORT || 4010;
 const greeting = process.env.GREETING;
 
-server.listen(4010, () => {
-  console.log(`\n*** ${greeting} Server Running on http://localhost:4010 ***\n`);
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} Server Running on http://localhost:${port} ***\n`);
 });
